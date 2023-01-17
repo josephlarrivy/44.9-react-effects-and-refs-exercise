e887ff239cea495a7446e81f55176b51fc1c9a72
josephlarrivy <joseph.larrivy@gmail.com>
2023-01-17 16:31:04 -0600

    can's figure out how to get interval to work right

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,10 @@
 import React, { useState, useRef, useEffect } from "react";
 
 const Card = (props) => {
-    
+
     return (
         <>
-            {/* <img src="https://deckofcardsapi.com/static/img/6H.png"></img> */}
-            <p>card</p>
+            <img src={props.imgSource}></img>
         </>
     )
 }
--- src/Stack.js
+++ src/Stack.js
@@ -5,52 +5,137 @@ import axios from 'axios'
 
 const Stack = () => {
 
-    const deck = useRef();
+    const [deck, setDeck] = useState();
     const [cards, setCards] = useState([]);
     const [displayCard, setDisplayCard] = useState();
+    const [autoDraw, setAutoDraw] = useState(false);
+    const [counter, setCounter] = useState(null);
+
 
 
     useEffect(function createNewDeck() {
-        async function start() {
-            let createDeck = await axios.get("https://deckofcardsapi.com/api/deck/new/")
-            deck.current = createDeck
 
-            console.log(`deck ${deck}`)
+        async function start() {
+            try {
+                let request = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle");
+                setDeck(request.data.deck_id)
+                console.log(deck)
+
+                let secondRequest = await axios.get(`https://deckofcardsapi.com/api/deck/${request.data.deck_id}/draw/?count=1`);
+                let drawnCard = secondRequest.data.cards[0]
+                setCards(cards => [...cards,
+                {
+                    id: drawnCard.code,
+                    image: drawnCard.image
+                }]);
+            } catch (e) {
+                return console.log(e)
+            }
         }
         start();
-        // draw();
-        console.log(`cards ${cards}`)
-        console.log(`displayCard ${displayCard}`)
+        console.log(deck)
+        console.log(cards)
+
     }, [])
 
+
     async function draw() {
-
-        let newCard = await axios.get(`https://deckofcardsapi.com/api/deck/${deck.current.data.deck_id}/draw/?count=1`);
-        console.log(newCard.data.cards[0]);
-
-        setCards(cards => [newCard.data.cards[0], ...cards])
-        setDisplayCard(cards[0])
-
-        console.log(`cards ${cards}`)
-        console.log(`displayCard ${displayCard}`)
+        try {
+            let request = await axios.get(`https://deckofcardsapi.com/api/deck/${deck}/draw/?count=1`);
+            let drawnCard = request.data.cards[0]
+            setCards(cards => [...cards,
+                {
+                    id: drawnCard.code,
+                    image: drawnCard.image
+                }]);
+            let cardToShow = cards[cards.length - 1];
+            setDisplayCard(cardToShow.image);
+        } catch (e) {
+            return console.log(e)
+        }
     }
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    // const [autoDraw, setAutoDraw] = useState(false);
+    // const [counter, setCounter] = useState(null);
+
+
+    useEffect(() => {
+        let interval;
+        if (autoDraw) {
+            interval = setInterval(() => {
+                console.log('In setInterval');
+                draw();
+            }, 1000);
+            setCounter(100)
+        } else {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [autoDraw]);
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     async function handleClick(evt) {
         evt.preventDefault();
         // const btn = evt.target
         await draw();
+        
+    }
+
+    const handleClick2 = () => {
+        setAutoDraw(on => !on);
+        console.log(autoDraw)
     }
 
 
-    return (
-        <>
-            <Card imgSource={displayCard.image}/>
-            <Card />
-
-            <button onClick={handleClick}>Draw Card</button>
-        </>
-    )
+{
+    if (cards.length == 52) {
+        return (
+            <>
+                <h1>Error: no cards remaining!</h1>
+            </>
+        )
+    } else {
+        return (
+            <div>
+                <Card imgSource={displayCard}/>
+                <br></br>
+                <button onClick={handleClick}>Draw Card</button>
+                <button onClick={handleClick2}>Automatically draw</button>
+            </div>
+        )
+    }
+}
 }
 
 
